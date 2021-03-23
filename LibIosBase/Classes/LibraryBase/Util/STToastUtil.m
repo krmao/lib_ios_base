47f68142a1712a91ed8bce87d9950cea72aa314c
kr.mao <kr.mao@ctrip.com>
2021-03-23 19:08:05 +0800

    test

--- LibIosBase/Classes/LibraryBase/Util/STToastUtil.m
+++ LibIosBase/Classes/LibraryBase/Util/STToastUtil.m
@@ -14,7 +14,7 @@
 @implementation STToastUtil
 
 + (void)show:(NSString *)message{
-    
+    NSString * fMessage = message; //@"dsjhaflkashdfklsafklasjflaskdjflkasfdlkasfalksfhaslkjfhaslkdsjhaflkashdfklsafklasjflaskdjflkasfdlkasfalksfhaslkjfhaslk";
     BOOL animation = YES;
     UIView *view = [STSystemUtil topViewController].view;
     
@@ -26,8 +26,9 @@
     hud.contentColor = [UIColor clearColor];
     hud.offset = CGPointMake(0.f, MBProgressMaxOffset);
     hud.mode = MBProgressHUDModeCustomView;
-    CGRect messageRect = [message boundingRectWithSize:CGSizeMake(MAXFLOAT, 20) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName: [UIFont systemFontOfSize:15.0]} context:nil];
-    hud.minSize = CGSizeMake(messageRect.size.width+30, 35);
+    CGRect messageRect = [STSystemUtil getTextSizeWithBoundingRect:fMessage withSize:CGSizeMake(0, 0) withFont:[UIFont systemFontOfSize:15]];
+    // CGRect messageRect = [fMessage boundingRectWithSize:CGSizeMake(MAXFLOAT, 20) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName: [UIFont systemFontOfSize:15.0]} context:nil];
+    hud.minSize = CGSizeMake(messageRect.size.width+30, messageRect.size.height + 15);
     hud.bezelView.color = [UIColor clearColor];
     hud.bezelView.backgroundColor = [UIColor clearColor];
     hud.bezelView.style = MBProgressHUDBackgroundStyleSolidColor;
@@ -35,11 +36,13 @@
     hud.bezelView.layer.backgroundColor = [[UIColor alloc] initWithHex:@"#333333"].CGColor;
     hud.bezelView.layer.cornerRadius = 15.0;
 
-    UILabel *titleLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 5, messageRect.size.width+30 ,30)];
-    titleLabel.text = message;
+    UILabel *titleLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 5, messageRect.size.width+30 , messageRect.size.height + 10)];
+    titleLabel.text = fMessage;
+    titleLabel.numberOfLines = 0;
     titleLabel.textAlignment = NSTextAlignmentCenter;
     titleLabel.font = [UIFont systemFontOfSize:15];
     titleLabel.textColor = [UIColor whiteColor];
+    titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
     [hud.bezelView addSubview:titleLabel];
     
     [hud setRemoveFromSuperViewOnHide:YES];
